9f429a82c32edd724746411c47ef6d3a38526a0d
Claudiu Stroe <claudiucls@gmail.com>
2026-02-24 17:04:23 +0200

    add exception handling

--- src/main/java/dev/claudiu_software/taskmanager/controller/TaskController.java
+++ src/main/java/dev/claudiu_software/taskmanager/controller/TaskController.java
@@ -27,10 +27,8 @@ public class TaskController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Task> getTaskById(@PathVariable Long id){
-        return taskService.getTaskById(id)
-                .map(ResponseEntity::ok)
-                .orElse(ResponseEntity.notFound().build());
+    public Task getTaskById(@PathVariable Long id){
+        return taskService.getTaskById(id);
 
     }
     @PostMapping
@@ -40,17 +38,15 @@ public class TaskController {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Task> updateTask(@PathVariable Long id, @RequestBody Task updatedTask ) {
-        return taskService.updateTask(id, updatedTask)
-                .map(ResponseEntity::ok)
-                .orElse(ResponseEntity.notFound().build());
+    public Task updateTask(@PathVariable Long id, @RequestBody Task updatedTask ) {
+        return taskService.updateTask(id, updatedTask);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteTask(@PathVariable Long id){
-        return taskService.deleteTask(id) ?
-                ResponseEntity.noContent().build() :
-                ResponseEntity.notFound().build();
+         taskService.deleteTask(id);
+         return ResponseEntity.ok().build();
+
     }
 
     @GetMapping("/completed/{status}")
